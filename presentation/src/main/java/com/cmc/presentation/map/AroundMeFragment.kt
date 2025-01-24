bafc3e5b7990dfc1aa607256fc4de7f1fdb14658
dogdduddy <dogdduddy@gmail.com>
2025-01-25 01:16:09 +0900

    feat: 지도 카테고리 필터 구현

--- presentation/src/main/java/com/cmc/presentation/map/AroundMeFragment.kt
+++ presentation/src/main/java/com/cmc/presentation/map/AroundMeFragment.kt
@@ -1,15 +1,40 @@
 package com.cmc.presentation.map
 
 import com.cmc.common.base.BaseFragment
+import com.cmc.common.model.SpotCategory
 import com.cmc.presentation.R
 import com.cmc.presentation.databinding.FragmentAroundMeBinding
+import com.naver.maps.map.MapView
 
 class AroundMeFragment: BaseFragment<FragmentAroundMeBinding>(R.layout.fragment_around_me) {
+
+    private lateinit var mapView: MapView
+
     override fun initObserving() {
 
     }
 
     override fun initView() {
+        mapView = binding.viewMap
+
+        setCategory()
+    }
+
+    private fun setCategory() {
+        val categoryTabs: List<Pair<Int, Int?>> = SpotCategory.entries
+            .map { it.stringId to it.resId}
+
+        binding.tabMapFilter.setTabList(categoryTabs)
+
+        mapView.getMapAsync { naverMap ->
+
+            naverMap.uiSettings.apply {
+                isZoomControlEnabled = false
+                isScaleBarEnabled = false
+                isLogoClickEnabled = false
+            }
+
+        }
 
     }
 }
