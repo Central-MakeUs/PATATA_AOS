a0b8089502868c0d6081e260280fcc411b9b2b15
dogdduddy <dogdduddy@gmail.com>
2025-01-28 00:16:36 +0900

    feat: 내 주변, 위치 권한 요청 추가

--- presentation/src/main/java/com/cmc/presentation/map/AroundMeFragment.kt
+++ presentation/src/main/java/com/cmc/presentation/map/AroundMeFragment.kt
@@ -1,23 +1,65 @@
 package com.cmc.presentation.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import com.cmc.common.base.BaseFragment
 import com.cmc.common.model.SpotCategory
 import com.cmc.presentation.R
 import com.cmc.presentation.databinding.FragmentAroundMeBinding
 import com.naver.maps.map.MapView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import com.cmc.presentation.map.AroundMeViewModel.AroundMeSideEffect
 
+@AndroidEntryPoint
 class AroundMeFragment: BaseFragment<FragmentAroundMeBinding>(R.layout.fragment_around_me) {
 
+    private val viewModel: AroundMeViewModel by viewModels()
     private lateinit var mapView: MapView
 
     override fun initObserving() {
+        repeatWhenUiStarted {
+            launch {
+                viewModel.state.collectLatest {
 
+                }
+            }
+
+            launch {
+                viewModel.sideEffect.collectLatest { effect ->
+                    when (effect) {
+                        is AroundMeSideEffect.RequestLocationPermission -> {
+                            checkLocationRequest()
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun initView() {
-        mapView = binding.viewMap
-
+        setMap()
         setCategory()
+        setButton()
+    }
+
+    private fun setMap() {
+        mapView = binding.viewMap
+        mapView.getMapAsync { naverMap ->
+            naverMap.uiSettings.apply {
+                isZoomControlEnabled = false
+                isScaleBarEnabled = false
+                isLogoClickEnabled = false
+            }
+
+            viewModel.checkLocationPermission()
+        }
     }
 
     private fun setCategory() {
@@ -25,16 +67,38 @@ class AroundMeFragment: BaseFragment<FragmentAroundMeBinding>(R.layout.fragment_
             .map { it.stringId to it.resId}
 
         binding.tabMapFilter.setTabList(categoryTabs)
-
-        mapView.getMapAsync { naverMap ->
-
-            naverMap.uiSettings.apply {
-                isZoomControlEnabled = false
-                isScaleBarEnabled = false
-                isLogoClickEnabled = false
-            }
-
+    }
+    
+    private fun setButton() {
+        binding.layoutAddLocation.setOnClickListener {
+            viewModel.checkLocationPermission()
+            // TODO: 장소 추가하기 화면으로 이동
         }
+        
+        binding.ivCurrentLocation.setOnClickListener {
+            // TODO: 현재 위치로 지도 이동하기 구현
+        }
+    }
 
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+
+        if (permissions.all { it.value }) {
+            Toast.makeText(context, "위치 권한이 허용되었습니다!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkLocationRequest() {
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        if (permissions.all {
+                ContextCompat.checkSelfPermission(requireContext(), it) != PackageManager.PERMISSION_GRANTED
+            }) {
+            locationPermissionRequest.launch(permissions)
+        }
     }
 }
--- presentation/src/main/java/com/cmc/presentation/map/AroundMeViewModel.kt
+++ presentation/src/main/java/com/cmc/presentation/map/AroundMeViewModel.kt
@@ -1,4 +1,50 @@
 package com.cmc.presentation.map
 
-class AroundMeViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AroundMeViewModel @Inject constructor(): ViewModel() {
+
+    private val _state = MutableStateFlow(TodaySpotRecommendationState())
+    val state: StateFlow<TodaySpotRecommendationState> = _state.asStateFlow()
+
+    private val _sideEffect = MutableSharedFlow<AroundMeSideEffect>()
+    val sideEffect: SharedFlow<AroundMeSideEffect> = _sideEffect.asSharedFlow()
+
+    fun checkLocationPermission() {
+        sendSideEffect(AroundMeSideEffect.RequestLocationPermission)
+    }
+
+    private fun sendSideEffect(effect: AroundMeSideEffect) {
+        viewModelScope.launch {
+            _sideEffect.emit(effect)
+        }
+    }
+
+    data class TodaySpotRecommendationState(
+        val results: Int? = null,
+        val errorMessage: String? = null,
+        val searchStatus: AroundMeStatus = AroundMeStatus.LOADING
+    )
+
+    enum class AroundMeStatus {
+        LOADING,
+        SUCCESS,
+        ERROR,
+    }
+
+    sealed class AroundMeSideEffect {
+        data object RequestLocationPermission : AroundMeSideEffect()
+    }
 }
